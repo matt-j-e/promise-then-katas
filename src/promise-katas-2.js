61eb329abcbb7ce505abf43a062656c02717ab69
mattedwards <the.matt.edwards@googlemail.com>
2021-03-02 15:07:13 +0000

    completed food, cat & dog of promise-katas-2

--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -25,17 +25,19 @@ fetch("joke", "question").then(<insert your callback function>)
 // 1 Create a function that uses the fetch function to make a request to the "food" URL and returns
 // the data - expected return value "Cheese" of type String
 
-const food = () => {};
+const food = () => fetch("food").then((object) => object.data);
 
 // 2 Create a function that uses the fetch function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order - expected return value ["Bandit", "Berry", "Puss in boots", "Smokey"] of type Array
 
-const cat = () => {};
+const cat = () => fetch("cats").then(object => object.data.cats.sort());
 
 // 3 Create a function that uses the fetch function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
-const dog = () => {};
+const dog = () => fetch("dogs")
+    .then(object => object.data.dogs
+    .sort((a, b) => a.naughty > b.naughty ? -1 : 1)[0]);
 
 // 4 Create a function that uses the fetch function to make requests to the "jokes" URL and returns
 // a joke object with the key of question and answer - expected return { 
